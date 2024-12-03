ef4d7c611c817b3446c60331349173431b06cb66
literaryverses <98240549+literaryverses@users.noreply.github.com>
2024-12-02 17:56:26 -0700

    adding right triangle integer

--- basic_logic/l1_patterns.py
+++ basic_logic/l1_patterns.py
@@ -1,13 +1,20 @@
 # 1 learn important patterns
 
 
-""" input=3, output=
-* * *
-* * *
-* * *"""
-
-
 def regular_block(n):
+    """
+    Prints a square block pattern of asterisks based on the input size.
+
+    Args:
+        n (int): The size of the square block.
+
+    Example:
+        Input: 3
+        Output:
+        * * *
+        * * *
+        * * *
+    """
     if n > 0:
         for _ in range(n):
             print("* " * n)
@@ -22,6 +29,49 @@ def regular_block(n):
 
 
 def right_triangle(n):
+    """
+    Prints a right triangle pattern of asterisks based on the input size.
+
+    Args:
+        n (int): The size of the right triangle.
+
+    Example:
+        input=5, output=
+        *
+        * *
+        * * *
+        * * * *
+        * * * * *
+    """
     if n > 0:
         for i in range(1, n + 1):
             print("* " * i)
+
+
+def right_triangle_integers_1(n):
+    """
+    Prints a right triangle pattern of numbers based on the input size.
+
+    Args:
+        n (int): The size of the right triangle.
+
+    Example:
+        input=5, output=
+        1
+        1 2
+        1 2 3
+        1 2 3 4
+        1 2 3 4 5
+    """
+    if n > 0:
+        for row in range(1, n + 1):
+            for col in range(1, row + 1):
+                if col != row:
+                    print(col, end=" ")
+                else:
+                    print(col)
+
+
+def right_triangle_integers_2(n):
+    for row in range(1, n + 1):
+        print(" ".join(map(str, range(1, row + 1))))
--- basic_logic/t1_patterns.py
+++ basic_logic/t1_patterns.py
@@ -1,7 +1,12 @@
 import unittest
 from io import StringIO
 from unittest.mock import patch
-from basic_logic.l1_patterns import regular_block, right_triangle
+from l1_patterns import (
+    regular_block,
+    right_triangle,
+    right_triangle_integers_2,
+    right_triangle_integers_1,
+)
 
 
 class TestRegularBlock(unittest.TestCase):
@@ -50,6 +55,71 @@ class TestRightTriange(unittest.TestCase):
     def test_right_triangle_negative(self, mock_stdout):
         right_triangle(-5)
         self.assertEqual(mock_stdout.getvalue(), "")
+
+
+class TestRightTriangleIntegers(unittest.TestCase):
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_right_triangle_positive(self, mock_stdout):
+        input_value = 5
+
+        right_triangle_integers_1(input_value)
+        output_1 = mock_stdout.getvalue()
+
+        mock_stdout.truncate(0)
+        mock_stdout.seek(0)
+
+        right_triangle_integers_2(input_value)
+        output_2 = mock_stdout.getvalue()
+
+        expected_output = "1\n1 2\n1 2 3\n1 2 3 4\n1 2 3 4 5\n"
+        self.assertTrue(output_1 == output_2 == expected_output)
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_right_triangle_zero(self, mock_stdout):
+        input_value = 0
+
+        right_triangle_integers_1(input_value)
+        output_1 = mock_stdout.getvalue()
+
+        mock_stdout.truncate(0)
+        mock_stdout.seek(0)
+
+        right_triangle_integers_2(input_value)
+        output_2 = mock_stdout.getvalue()
+
+        self.assertTrue(output_1 == output_2 == "")
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_right_triangle_one(self, mock_stdout):
+        input_value = 1
+
+        right_triangle_integers_1(input_value)
+        output_1 = mock_stdout.getvalue()
+
+        mock_stdout.truncate(0)
+        mock_stdout.seek(0)
+
+        right_triangle_integers_2(input_value)
+        output_2 = mock_stdout.getvalue()
+
+        expected_output = "1\n"
+
+        self.assertTrue(output_1 == output_2 == expected_output)
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_right_triangle_negative(self, mock_stdout):
+        input_value = -5
+
+        right_triangle_integers_1(input_value)
+        output_1 = mock_stdout.getvalue()
+
+        mock_stdout.truncate(0)
+        mock_stdout.seek(0)
+
+        right_triangle_integers_2(input_value)
+        output_2 = mock_stdout.getvalue()
+
+        self.assertTrue(output_1 == output_2 == "")
 
 
 if __name__ == "__main__":
