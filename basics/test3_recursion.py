c15fb539de991ecdeec65f7ada1ea3e4873a5383
literaryverses <98240549+literaryverses@users.noreply.github.com>
2024-12-04 13:34:54 -0700

    completed Counter

--- basics/test3_recursion.py
+++ basics/test3_recursion.py
@@ -4,7 +4,7 @@ import sys
 from lec3_recursion import *
 
 
-class TestRepeat:
+class TestRepeat(unittest.TestCase):
     def setUp(self):
         # redirect stdout to capture print statements
         self.held_output = StringIO()
@@ -30,13 +30,64 @@ class TestRepeat:
         with self.assertRaises(RecursionError):
             Repeat.solution("negative", -1)
 
-    def test_invalid_word_type(self):
-        with self.assertRaises(AttributeError):
-            Repeat.solution(123, 3)
 
+class TestCounter(unittest.TestCase):
+    def setUp(self):
+        self.held_output = StringIO()
+        sys.stdout = self.held_output
 
-class TestCounter:
-    pass
+    def tearDown(self):
+        sys.stdout = sys.__stdout__
+
+    def test_solution_basic(self):
+        Counter.solution(5)
+        self.held_output.seek(0)
+        output = self.held_output.read().strip().split("\n")
+        self.assertEqual(output, ["1", "2", "3", "4", "5"])
+
+    def test_solution_n_zero(self):
+        Counter.solution(0)
+        self.held_output.seek(0)
+        output = self.held_output.read()
+        self.assertEqual(output, "")
+
+    def test_solution_negative_n(self):
+        Counter.solution(-5)
+        self.held_output.seek(0)
+        output = self.held_output.read()
+        self.assertEqual(output, "")
+
+    def test_solution_large_n(self):
+        n = 100
+        Counter.solution(n)
+        self.held_output.seek(0)
+        output = self.held_output.read().strip().split("\n")
+        self.assertEqual(output, [str(i) for i in range(1, n + 1)])
+
+    def test_backtracking_basic(self):
+        Counter.backtracking(5)
+        self.held_output.seek(0)
+        output = self.held_output.read().strip().split("\n")
+        self.assertEqual(output, ["1", "2", "3", "4", "5"])
+
+    def test_backtracking_n_zero(self):
+        Counter.backtracking(0)
+        self.held_output.seek(0)
+        output = self.held_output.read()
+        self.assertEqual(output, "")
+
+    def test_backtracking_negative_n(self):
+        Counter.backtracking(-5)
+        self.held_output.seek(0)
+        output = self.held_output.read()
+        self.assertEqual(output, "")
+
+    def test_backtracking_large_n(self):
+        n = 100
+        Counter.backtracking(n)
+        self.held_output.seek(0)
+        output = self.held_output.read().strip().split("\n")
+        self.assertEqual(output, [str(i) for i in range(1, n + 1)])
 
 
 class TestCounterBackwards:
