f5a34ee7888528c39169050a6caaae52a360e7fb
literaryverses <98240549+literaryverses@users.noreply.github.com>
2024-12-04 15:02:03 -0700

    Sums and Factorials

--- basics/lec3_recursion.py
+++ basics/lec3_recursion.py
@@ -12,7 +12,7 @@ class Repeat:
 
 
 class Counter:
-    """Print 1 to n"""
+    """Print 1 to n."""
 
     # O(n) time complexity
     # O(n) space complexity
@@ -33,8 +33,10 @@ class Counter:
 
 
 class CounterBackwards:
-    """Print n to 1"""
+    """Print n to 1."""
 
+    # O(n) time complexity
+    # O(1) space complexity
     @staticmethod
     def solution(n):
         if n < 1:
@@ -42,6 +44,8 @@ class CounterBackwards:
         print(n)
         return CounterBackwards.solution(n - 1)
 
+    # O(n) time complexity
+    # O(1) space complexity
     @staticmethod
     def backtracking(n, current=1):
         if n < 0:
@@ -53,15 +57,71 @@ class CounterBackwards:
 
 
 class Sums:
-    pass
+    """Sum of the first n natural numbers."""
+
+    # O(n) time complexity
+    # O(1) space complexity
+    @staticmethod
+    def iterative(n):
+        sum = 0
+        for i in range(1, n + 1):
+            sum += i
+        return sum
+
+    # O(1) time complexity
+    # O(1) space complexity
+    @staticmethod
+    def formula(n):
+        """arithmetic series"""
+        if n < 0:
+            return 0
+        return n * (n + 1) // 2
+
+    # O(n) time complexity
+    # O(n) space complexity
+    @staticmethod
+    def recursive_parameterized(n, sum=0):
+        if n <= 0:
+            return sum
+        return Sums.recursive_parameterized(n - 1, sum + n)
+
+    # O(n) time complexity
+    # O(n) space complexity
+    @staticmethod
+    def recursive_functional(n):
+        if n <= 0:
+            return 0
+        return n + Sums.recursive_functional(n - 1)
 
 
 class Factorials:
-    pass
+    """Factorial of a given number."""
+
+    # O(n) time complexity
+    # O(1) space complexity
+    @staticmethod
+    def iterative(n):
+        if n < 0:
+            raise ValueError("Factorial is not defined for negative numbers.")
+
+        product = 1
+        for i in range(1, n + 1):
+            product *= i
+        return product
+
+    @staticmethod
+    # O(n) time complexity
+    # O(n) space complexity
+    def recursive(n):
+        if n < 0:
+            raise ValueError("Factorial is not defined for negative numbers.")
+        if n == 1:
+            return 1
+        return n * Factorials.recursive(n - 1)
 
 
-class Reverse:
-    pass
+class ReverseArray:
+    """Reverses an array."""
 
 
 class Palindrome:
